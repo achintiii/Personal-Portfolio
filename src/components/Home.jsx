2e7a90eac5708a121cbeaa3bcdd1bd2f344cefbe
achintiii <achintiii@outlook.com>
2024-11-15 14:05:57 -0500

    working on home screen right now

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,8 +3,19 @@ import {HiArrowNarrowRight} from 'react-icons/hi'
 
 const Home = () => {
   return (
-    <div className = 'w-full h-screen bg-[#0a192f]'>
-    
+    <div name = 'home' className= 'w-full h-screen bg-[#0a192f]'>
+        {/* Container*/}
+        <div className = 'max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
+            <p className='text-teal-300'> Hi, my name is</p>
+            <h1 className = 'text-4xl sm: text-7xl font bold text-[#ccd6f6]'> Ashwin Chintalapati</h1>
+            <h2> I am a full stack developer </h2>
+            <p>
+            specializing in building AI applications. Currently, I am focusing on building large scale responsive web applications
+            </p>
+            <div>
+                <button> View Work <HiArrowNarrowRight /> </button>
+            </div>
+            </div>
     </div>
   )
 }
